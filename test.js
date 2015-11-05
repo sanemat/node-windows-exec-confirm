a2bb778a7ebfba16dd12372b31812842605f9fec
sanemat <o.gata.ken@gmail.com>
2015-11-05 17:17:45 +0900

    chore(touch1): use console.error

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var spawn = require('child_process').spawn;
 var touch1 = spawn('npm', ['run', 'touch1'], { stdio: 'inherit' });
 touch1.on('error', function(err) {
-  console.log(err);
+  console.error(err);
 });
