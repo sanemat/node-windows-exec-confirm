c8b89f780deced598c3ccbe27ce70d78d5f5abcb
sanemat <o.gata.ken@gmail.com>
2015-11-05 17:47:21 +0900

    chore(error): exit 1

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ var spawn = require('child_process').spawn;
 var touch1 = spawn('npm', ['run', 'touch1'], { stdio: 'inherit' });
 touch1.on('error', function(err) {
   console.error(err);
+  process.exit(1);
 });
