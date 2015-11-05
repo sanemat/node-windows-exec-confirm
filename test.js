f72fc23c5ca6d4b76705885521865d897ba651e8
sanemat <o.gata.ken@gmail.com>
2015-11-05 17:58:15 +0900

    chore(npm): add verbose

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var spawn = require('child_process').spawn;
-var touch1 = spawn('npm', ['run', 'touch1'], { stdio: 'inherit' });
+var touch1 = spawn('npm', ['run', 'touch1', '--verbose'], { stdio: 'inherit' });
 touch1.on('error', function(err) {
   console.error(err);
   process.exit(1);
