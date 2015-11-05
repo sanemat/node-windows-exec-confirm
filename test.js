24381fa63c4fc65a32577448d87e8b8b671867af
sanemat <o.gata.ken@gmail.com>
2015-11-06 00:31:37 +0900

    chore(which): show which npm

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 var spawn = require('child_process').spawn;
+var which = require('which');
+
 console.log('process.cwd()');
 console.log(process.cwd());
 console.log('process.env.PATH');
 console.log(process.env.PATH);
+
+which('npm', function (error, resolvedPath) {
+  if (error) {
+    console.error('which npm: error');
+    console.error(error);
+    return;
+  }
+  console.log('which npm:');
+  console.log(resolvedPath);
+});
+
 var touch1 = spawn('npm', ['run', 'touch1', '--verbose'], { stdio: 'inherit' });
 touch1.on('error', function(err) {
   console.error(err);
