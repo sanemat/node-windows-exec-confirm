d7aaa8f230c4cb17bff9216ac42bee590bb7063a
sanemat <o.gata.ken@gmail.com>
2015-11-06 19:26:18 +0900

    chore(iswin): windows use npm.cmd

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 var spawn = require('child_process').spawn;
+var iswin = require('iswin');
 console.log('process.cwd()');
 console.log(process.cwd());
 console.log('process.env.PATH');
 console.log(process.env.PATH);
-var touch1 = spawn('npm', ['run', 'touch1', '--verbose'], { stdio: 'inherit' });
+var cmd = (iswin()) ? 'npm.cmd' : 'npm';
+var touch1 = spawn(cmd, ['run', 'touch1', '--verbose'], { stdio: 'inherit' });
 touch1.on('error', function(err) {
   console.error(err);
   process.exit(1);
